87f0c74bf21a3c0fc2079ce75d9092efd03917c4
Sohel Amin <sohelamincse@gmail.com>
2017-02-06 12:35:53 +0600

    Upgraded to 5.4

--- README.md
+++ README.md
@@ -7,15 +7,21 @@ git clone https://github.com/sohelamin/laravel-starter.git
 cd laravel-starter
 composer install
 npm install
-gulp
+php -r "copy('.env.example', '.env');"
+php artisan key:generate
 php artisan migrate
 php artisan db:seed
+
+npm run dev
+php artisan serve
 ```
 
 ## Login
 
+```
 User: admin@example.com
 Pass: 123456
+```
 
 ## Screenshot
 ![screenshot](https://cloud.githubusercontent.com/assets/1708683/19011977/9a2c7a18-87c8-11e6-8c59-a0cbc3916b9f.png)
--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -8,6 +8,7 @@ window._ = require('lodash');
  */
 
 window.$ = window.jQuery = require('jquery');
+
 require('bootstrap-sass');
 
 /**
@@ -18,10 +19,18 @@ require('bootstrap-sass');
 
 window.Vue = require('vue');
 
-window.axios = require('axios').create({
-    headers: {'X-CSRF-TOKEN': Laravel.csrfToken}
-});
+/**
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
+ */
 
+window.axios = require('axios');
+
+window.axios.defaults.headers.common = {
+    'X-CSRF-TOKEN': window.Laravel.csrfToken,
+    'X-Requested-With': 'XMLHttpRequest'
+};
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
